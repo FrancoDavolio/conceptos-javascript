d12a189fd08169ddad17dfdc5b98795ccc82e639
Franco Davolio <francodavolio12@gmail.com>
2022-06-29 11:15:23 -0300

    agregamos el codigo para mostrar  dif mensajes

---
+++ js/01-variables.js
@@ -0,0 +1,11 @@
+//mostrar un mensaje en el documento html
+document.write("<h1>hola franco</h1>");
+
+//mostrar un mensaje en la consola
+console.log("nueva mensaje");
+
+//mostrar un mensaje en una venmtana emergente
+alert("prueba de mensjae en ventana emergente");
+
+//mostrar una suma
+document.write("resultado: " + (29 + 6));
