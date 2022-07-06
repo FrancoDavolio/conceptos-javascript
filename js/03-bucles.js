5a9d2f0bf88c2343496e1c5638ec3f3b64b3e4c8
Franco Davolio <francodavolio12@gmail.com>
2022-07-06 11:30:05 -0300

    bucle while

---
+++ js/03-bucles.js
@@ -0,0 +1,10 @@
+/* while (condicion logica){
+    todo el codigo que quiero que se repita 
+    cambiar la condicon logica
+}*/
+let cantidaMates = 1;
+
+while(cantidaMates <= 20){
+    document.write("<br>Mates cebados: " + cantidaMates);
+    cantidaMates++;// cantidaMates = cantidaMates + 1;
+} 
