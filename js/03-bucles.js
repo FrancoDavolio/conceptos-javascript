b72821beb144857c6975b106a16edd294fe7a0a2
Franco Davolio <francodavolio12@gmail.com>
2022-07-11 09:58:53 -0300

    bucle do-while

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -2,9 +2,26 @@
     todo el codigo que quiero que se repita 
     cambiar la condicon logica
 }*/
+/*
 let cantidaMates = 1;
 
 while(cantidaMates <= 20){
     document.write("<br>Mates cebados: " + cantidaMates);
     cantidaMates++;// cantidaMates = cantidaMates + 1;
 } 
+*/
+
+/* do-while, hacer...mientras (se cumpla una condicion logica);
+
+do {
+    todas las lineas de codigo a ejecutar
+    cambiar la condicion logica 
+}while(condicion logica);
+*/
+
+let cantidaMates = 20
+
+do{
+    document.write("<br>Mates cebados: N " + cantidaMates);
+    cantidaMates--;
+}while(cantidaMates >=)
